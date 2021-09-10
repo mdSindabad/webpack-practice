8c42b12f1a13a5c7db4cea2fd5e4cf537c5c0af7
Md. Sindabad <pavel.sindabad@gmail.com>
2021-09-10 11:29:53 +0600

    added postcss

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
             }
         ]
     },
